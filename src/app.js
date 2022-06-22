7283add89fc9886ef7407fa45abfbdea88961080
Julia Rodrigues <rodriguesjulia.amaral@gmail.com>
2022-06-21 22:01:22 -0300

    Buscar livro

--- src/app.js
+++ src/app.js
@@ -17,6 +17,11 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req, res) =>  { 
+    let index = buscaLivro(req.params.id)
+    res.json(livros[index])
+})
+
 app.post('/livros', (req, res) =>  { 
     livros.push(req.body)
     res.status(201).send('Livro foi cadastrado com sucesso')
